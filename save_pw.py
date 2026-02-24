5e193b9cb5e1fb3fb0cf4f66dc1f1c8b257fb4d8
ArtemZhov <artem-zhov@yandex.ru>
2026-02-24 12:31:33 +0300

    Delete save_pw.py

--- save_pw.py
+++
@@ -1,5 +0,0 @@
-import getpass, keyring
-username = "artem.zhovtonozhko@rt.ru"
-pw = getpass.getpass(f"Введите пароль для {username}: ")
-keyring.set_password("my_email_app", username, pw)
-print("Пароль сохранён.")
